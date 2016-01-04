7385130c86039f99d6e9960dc0c2a38567642a51
MW <nefrius@gmail.com>
2016-01-04 14:38:28 +0100

    PathBuilders added

---
+++ tests/unit/TestDebugPathBuilder.py
@@ -0,0 +1,62 @@
+import os
+import imp
+import unittest
+
+path = fullPath = os.path.realpath(__file__)
+projectPath= os.path.dirname(os.path.dirname(os.path.dirname(fullPath)))
+fullPath = os.path.join(projectPath, "src", "PathBuilder", "DebugPathBuilder.py")
+p = imp.load_source("DebugPathBuilder", fullPath)
+
+
+class TestDebugPathBuilder(unittest.TestCase):
+    def test_resourcePath(self):
+        self.assertEqual(p.DebugPathBuilder.resourcePath(), "/home/marek/SODM/res")
+        try:
+            p.DebugPathBuilder.resourcePath()
+        except NotImplementedError:
+            self.fail("Not implemented error.")
+
+    def test_testsPath(self):
+        self.assertEqual(p.DebugPathBuilder.testsPath(), "/home/marek/SODM/tests")
+        try:
+            p.DebugPathBuilder.testsPath()
+        except NotImplementedError:
+            self.fail("Not implemented error.")
+
+    def test_confPath(self):
+        self.assertEqual(p.DebugPathBuilder.confPath(), "/home/marek/SODM/tests/conf")
+        try:
+            p.DebugPathBuilder.confPath()
+        except NotImplementedError:
+            self.fail("Not implemented error.")
+
+    def test_unitPath(self):
+        self.assertEqual(p.DebugPathBuilder.unitPath(), "/home/marek/SODM/tests/unit")
+        try:
+            p.DebugPathBuilder.unitPath()
+        except NotImplementedError:
+            self.fail("Not implemented error.")
+
+    def test_srcPath(self):
+        self.assertEqual(p.DebugPathBuilder.srcPath(), "/home/marek/SODM/src")
+        try:
+            p.DebugPathBuilder.srcPath()
+        except NotImplementedError:
+            self.fail("Not implemented error.")
+
+    def test_pathPath(self):
+        self.assertEqual(p.DebugPathBuilder.pathPath(), "/home/marek/SODM/src/PathBuilder")
+        try:
+            p.DebugPathBuilder.pathPath()
+        except NotImplementedError:
+            self.fail("Not implemented error.")
+    def test_appendPath(self):
+        self.assertEqual(p.DebugPathBuilder.appendPath("abc", "bca"), "abc/bca")
+        self.assertEqual(p.DebugPathBuilder.appendPath(p.DebugPathBuilder.srcPath(), "abc"), p.DebugPathBuilder.srcPath() + "/abc")
+        try:
+            p.DebugPathBuilder.pathPath()
+        except NotImplementedError:
+            self.fail("Not implemented error.")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/unit/TestPathBuilder.py
@@ -0,0 +1,40 @@
+import os
+import imp
+import unittest
+
+path = fullPath = os.path.realpath(__file__)
+projectPath= os.path.dirname(os.path.dirname(os.path.dirname(fullPath)))
+fullPath = os.path.join(projectPath, "src", "PathBuilder", "PathBuilder.py")
+p = imp.load_source("PathBuilder", fullPath)
+
+
+class TestPathBuilder(unittest.TestCase):
+    def test_resourcePath(self):
+        with self.assertRaises(NotImplementedError):
+            p.PathBuilder.resourcePath()
+
+    def test_testsPath(self):
+        with self.assertRaises(NotImplementedError):
+            p.PathBuilder.testsPath()
+
+    def test_confPath(self):
+        with self.assertRaises(NotImplementedError):
+            p.PathBuilder.confPath()
+
+    def test_unitPath(self):
+        with self.assertRaises(NotImplementedError):
+            p.PathBuilder.confPath()
+
+    def test_srcPath(self):
+        with self.assertRaises(NotImplementedError):
+            p.PathBuilder.srcPath()
+
+    def test_pathPath(self):
+        with self.assertRaises(NotImplementedError):
+            p.PathBuilder.pathPath()
+    def test_appendPath(self):
+        with self.assertRaises(NotImplementedError):
+            p.PathBuilder.appendPath()
+
+if __name__ == "__main__":
+    unittest.main()
